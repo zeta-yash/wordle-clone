f0ce1de816d910b33a6de5e6083d3b7d778d9dbf
zeta-yash <somgupta070703@gmail.com>
2025-12-24 13:49:18 +0530

    style update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let inputtxt = document.querySelectorAll("input")[0];
 
 function boxColor(line1) {
     if (!line1) return;
-    
+
     const letters1 = line1.querySelectorAll("p");
     const boxes = line1.querySelectorAll("span");
 
@@ -17,25 +17,25 @@ function boxColor(line1) {
         if (letter === mainword[j]) {
             boxes[j].style.backgroundColor = "green";
         } else if (mainword.includes(letter)) {
-            boxes[j].style.backgroundColor = "khaki";
+            boxes[j].style.backgroundColor = "gold";
         } else {
             boxes[j].style.backgroundColor = "gray";
         }
 
-        
+
     }
 }
 
 
 function execute() {
-    
+
     let text = inputtxt.value.toUpperCase();
     const line = document.getElementById(`line${total_elements + 1}`);
     if (!line) return;
 
     const letters = line.querySelectorAll("p");
-    
-    if (text.length < 5){
+
+    if (text.length < 5) {
         inputtxt.value = "";
         alert("Only 5 letter words are accepted.")
         return;
@@ -47,31 +47,31 @@ function execute() {
         letters[i].textContent = text[i] || "";
     }
 
-    
+
     boxColor(line);
     entered_values.push(text);
     total_elements++;
 
     inputtxt.value = "";
-    
 
-    if (text===mainword){
+
+    if (text === mainword) {
         alert(`Congratulations!! You guessed the word in ${total_elements} attempt(s)`);
     }
-    else if (total_elements === 6 && text != mainword){
+    else if (total_elements === 6 && text != mainword) {
         alert(`You Lose. The correct word is ${mainword}`)
     }
-    
+
 }
 
 
 
 // Execute a function when the user presses a key on the keyboard
-inputtxt.addEventListener("keydown", function(event) {
-  // If the user presses the "Enter" key on the keyboard
-  if (event.key === "Enter") {
-    // Call your custom function or trigger a button click
-    // document.getElementById("submitbtn").click();
-    execute();
-  }
+inputtxt.addEventListener("keydown", function (event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        // Call your custom function or trigger a button click
+        // document.getElementById("submitbtn").click();
+        execute();
+    }
 });
